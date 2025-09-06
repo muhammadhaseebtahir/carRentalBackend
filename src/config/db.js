5c0a11a4c76f8ce9b0775b90f29ba0be81897d7b
muhammadhassebtahir <hassebtahir7860@gmail.com>
2025-09-06 19:46:07 +0500

    complete

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose")
 
-require("dotenv").config()
+// require("dotenv").config()
 
 const dbConnected = ()=>{
-    mongoose.connect(process.env.MONGO_URL,{
+    mongoose.connect("mongodb+srv://malikhaseeb456070:A97xZiknmsHiOoSH@cluster0.gly3v.mongodb.net/",{
         dbName:"carRental"
     }).then(()=>{
         console.log("Mongo  DB is connected")
@@ -12,5 +12,21 @@ const dbConnected = ()=>{
     })
 }
 module.exports= dbConnected
+
+
+// const mongoose = require("mongoose")
+
+// require("dotenv").config()
+
+// const dbConnected = ()=>{
+//     mongoose.connect(process.env.MONGO_URL,{
+//         dbName:"carRental"
+//     }).then(()=>{
+//         console.log("Mongo  DB is connected")
+//     }).catch((err)=>{
+//         console.log("connecting error",err)
+//     })
+// }
+// module.exports= dbConnected
 
 
